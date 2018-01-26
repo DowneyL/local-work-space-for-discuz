3b0b5ad3ff921a322b0b7ce76360e2628287fbe0
aragakiyui <heng8050lee@foxmail.com>
2018-01-26 08:51:58 +0800

    微信小程序开发

---
+++ wmapi/login.php
@@ -0,0 +1,32 @@
+<?php
+
+require 'inc.php';
+
+$token = $_POST['token'];
+$result = WmApiLib::check_token($token);
+$openid = $result['openid'];
+
+if (!function_exists('uc_user_login')) {
+    loaducenter();
+}
+$username = getDataForCharset($_POST['username']);
+$password = $_POST['password'];
+
+//用户登录验证
+$ucresult = uc_user_login($username, $password, 0, 0, '', '');
+$tmp = array();
+list($tmp['uid'], $tmp['username'], $tmp['password'], $tmp['email'], $duplicate) = daddslashes($ucresult, 1);
+$ucresult = $tmp;
+$uid = $ucresult['uid'];
+
+if ($uid < 0) {
+    WmApiError::display_result('user_login_failed');
+    exit();
+}
+
+$data = array();
+$data['token'] = WmApiLib::get_token($uid, $openid);
+$data['token_expire'] = 7200;
+
+WmApiError::display_result('ok', $data);
+
---
+++ wmapi/wmapi_config.php
@@ -0,0 +1,5 @@
+<?php
+define('WMAPI_KEY', 'u0nodnttzdp71yxl');
+define('WMAPI_KEY_EXPIRY', 7200);
+define('WMAPI_APPID', 'wxaaf313d8ec8c4a86');
+define('WMAPI_APPSECRET', '796f5575fed8ecac1ea0fd3fb8200717');
